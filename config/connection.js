b90bf6b9b1f096a4a3fc9060fa2218279f92f6dc
Christa Lococo <montecitocandleco@gmail.com>
2023-11-08 13:31:11 -0700

    heroku link

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-};
 
+  sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_localhost,
+    username: process.env.DB_root,
+    password: process.env.DB_Password,
+    database: process.env.DB_techBlog,
+    port: 3306,
+  });
+}
 
 module.exports = sequelize;
