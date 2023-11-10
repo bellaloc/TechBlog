759ec5bba6263d37a5f08498674a80b69a11957f
Christa Lococo <montecitocandleco@gmail.com>
2023-11-10 15:18:15 -0700

    routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeController');
-const dashboardRoutes = require('.dashboardRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 
 router.use('/', homeRoutes);
