c3869e430d4aeb81fdc58bc8afc43d476631d647
Christa Lococo <montecitocandleco@gmail.com>
2023-11-10 14:26:15 -0700

    home controllers

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,24 +1,67 @@
-const db = require('.././models');
+const {Post, Comment, User}= require('.././models');
 const router = require('express').Router();
 
+//get all posts for homepage
+router.get('/', async (req, res) => {
+ try {
+  const postData = await Post.findAll({
+    include: [User]
+  });
 
-// const homeController = {
-//   // Get all posts for the home page
-//   getAllPosts: async (req, res) => {
-//     try {
-//       const posts = await db.Post.findAll({
-//         include: { model: db.User, as: 'author' },
-//       });
-//       res.render('home', { posts });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     }
-//   },
-// };
+const posts = postData.map((post) => post.get({
+  plain: true
+}));
 
-router.get('/', (req, res) => {
-  res.render('main')
+res.render("all-posts", {posts})
+
+
+ } catch (error) {
+  res.status(500).json(error)
+ }
+});
+
+//get a single post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPK(req.params.id, {
+      include: [User,{
+        model: Comment, 
+        include: [User]
+      }]
+    });
+
+    if (postData){
+      const post = postData.get({
+        plain: true
+      });
+
+      res.render("single-post", {post})
+
+    } else {
+      res.status(404).end()
+
+    }
+
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
