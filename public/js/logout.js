e525732c1f34aab8e17fc189dda5b04cdf8b9897
Christa Lococo <montecitocandleco@gmail.com>
2023-11-13 16:22:51 -0700

    deleted dashboard.js, updated logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,19 @@
 const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
+  try {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert(response.statusText);
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(`Logout failed: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred during logout. Please try again.');
   }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logout-link').addEventListener('click', logout);
